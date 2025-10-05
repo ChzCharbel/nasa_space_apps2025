c5b599867c9141175b8f8b67876b727bf3a8d298
eduardo-hdez <ehdez2004@gmail.com>
2025-10-05 10:43:28 -0600

    [Fix] PlanetaryBackground added again

--- front-2/src/pages/dashboard/index.jsx
+++ front-2/src/pages/dashboard/index.jsx
@@ -4,6 +4,7 @@ import AstronomicalDataInput from "./componentes/AstronomicalDataInput";
 import DatasetActionButtons from "./componentes/DatasetActionButtons";
 import DatasetTable from "./componentes/DatasetTable";
 import Results from "./componentes/Results";
+import PlanetaryBackground from "./componentes/PlanetaryBackground";
 import "./styles.css";
 
 const Dashboard = () => {
@@ -213,6 +214,7 @@ const Dashboard = () => {
   return (
     <main>
       <NavBar />
+      <PlanetaryBackground />
       <div className="container" style={{ padding: "2rem 0" }}>
         {/* Input Form with Action Buttons */}
         <div className="input-section">
